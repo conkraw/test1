f1db0d33f23bb03565ed68eb4b5cfc70a4d65a79
conkraw <conkraw@gmail.com>
2024-09-26 12:06:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,23 +22,28 @@ def main():
     else:
         st.write("Welcome! Please enter your unique code to access the assessment.")
 
-        # Prompt user for unique code
-        unique_code = st.text_input("Unique Code:")
+        # Create columns for input
+        col1, col2 = st.columns([2, 1])  # Adjust the proportions as needed
 
-        if st.button("Next"):
-            if unique_code:
-                try:
-                    unique_code = int(unique_code.strip())  # Convert input to integer
-                    if unique_code in users['code'].values:
-                        st.session_state.user_name = users.loc[users['code'] == unique_code, 'name'].values[0]
-                        # Rerun to display the welcome page
-                        st.rerun()
-                    else:
-                        st.error("Invalid code. Please try again.")
-                except ValueError:
-                    st.error("Please enter a valid code.")
-            else:
-                st.error("Please enter a code.")
+        with col1:
+            unique_code = st.text_input("Unique Code:", placeholder="Enter your unique code")
+
+        with col2:
+            if st.button("Next"):
+                if unique_code:
+                    try:
+                        unique_code = int(unique_code.strip())  # Convert input to integer
+                        if unique_code in users['code'].values:
+                            st.session_state.user_name = users.loc[users['code'] == unique_code, 'name'].values[0]
+                            # Rerun to display the welcome page
+                            st.rerun()
+                        else:
+                            st.error("Invalid code. Please try again.")
+                    except ValueError:
+                        st.error("Please enter a valid code.")
+                else:
+                    st.error("Please enter a code.")
 
 if __name__ == "__main__":
     main()
+
