d00b0bde35452fc627ed3a1031b4967a601d7f4a
conkraw <conkraw@gmail.com>
2024-09-26 13:02:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def welcome_page():
     
     if st.button("Next"):
         st.session_state.page = "login"  # Change to login page
-        st.experimental_rerun()  # Rerun to refresh the view
+        st.rerun()  # Rerun to refresh the view
 
 # Login page function
 def login_page(users):
@@ -65,7 +65,7 @@ def login_page(users):
                 if unique_code in users['code'].values:
                     st.session_state.user_name = users.loc[users['code'] == unique_code, 'name'].values[0]
                     st.session_state.page = "assessment"  # Change to assessment page
-                    st.experimental_rerun()  # Rerun to refresh the view
+                    st.rerun()  # Rerun to refresh the view
                 else:
                     st.error("Invalid code. Please try again.")
             except ValueError:
