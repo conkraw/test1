cdb5f0eb036e09d6278eb96bd0b12322814b4867
conkraw <conkraw@gmail.com>
2024-09-28 20:06:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from firebase_setup import initialize_firebase, upload_to_firebase
+from firebase_setup import initialize_firebase
 from user_management import load_users, welcome_page, login_page
 from assessment import display_assessment
 from diagnoses import display_diagnoses
 from intervention import upload_intervention
 from virtual_patient import run_virtual_patient_app
-from focused_physical_exam import focused_physical_exam  # Import the new function
 
 def main():
     st.set_page_config(layout="wide")
@@ -22,7 +21,6 @@ def main():
         st.session_state.diagnoses = [""] * 5
         st.session_state.selected_buttons = [False] * 5
         st.session_state.assessment_data = {}
-        st.session_state.intervention_entry = None  # Initialize intervention entry
 
     # Check which page to display
     if st.session_state.page == "assessment":
@@ -34,14 +32,9 @@ def main():
     elif st.session_state.page == "diagnoses":
         display_diagnoses()
     elif st.session_state.page == "intervention":
-        upload_intervention()  # Just collects data, does not upload yet
+        upload_intervention()  # Calls the updated upload_intervention function
     elif st.session_state.page == "virtual_patient":
-        run_virtual_patient_app()  # Call the virtual patient app
-        st.session_state.page = "focused_physical_exam"  # Set the next page
-
-    # Handle focused physical examination selection
-    if st.session_state.page == "focused_physical_exam":
-        focused_physical_exam()
+        run_virtual_patient_app()
 
 if __name__ == "__main__":
     main()
