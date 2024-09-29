aff0028bc46d552f9c8f100125648c912875ec44
conkraw <conkraw@gmail.com>
2024-09-28 21:00:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import streamlit as st
-from utils.file_operations import load_users, read_text_file, load_vital_signs, read_diagnoses_from_file
-from utils.firebase_operations import upload_to_firebase
-from utils.user_management import welcome_page, login_page, display_assessment, display_diagnoses
+from utils.file_operations import load_users
+from utils.user_management import welcome_page, login_page
+from utils.assessment import display_assessment
+from utils.diagnosis import display_diagnoses
 
-# Main app function
 def main():
     st.title("Pediatric Clerkship Virtual Clinical Reasoning Assessment")
 
     # Load user data
     users = load_users()
 
-    # Initialize session state for page if not already done
+    # Initialize session state for the page if not already done
     if "page" not in st.session_state:
-        st.session_state.page = "welcome"  # Start on the welcome page
-        st.session_state.diagnoses = [""] * 5  # Initialize empty diagnoses
-        st.session_state.selected_buttons = [False] * 5  # Track selection status
-        st.session_state.assessment_data = {}  # Store assessment data
+        st.session_state.page = "welcome"
+        st.session_state.diagnoses = [""] * 5
+        st.session_state.selected_buttons = [False] * 5
+        st.session_state.assessment_data = {}
 
     # Check which page to display
     if st.session_state.page == "assessment":
