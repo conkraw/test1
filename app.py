28c50ca735c297032f9326905ac346292e208410
conkraw <conkraw@gmail.com>
2024-09-28 21:14:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,21 @@ import streamlit as st
 from utils.file_operations import load_users
 from utils.welcome import welcome_page
 from utils.login import login_page
+# Add other necessary imports
 
 st.set_page_config(layout="wide")
 
 def main():
-    # Load user data
+    # Your main logic here
     users = load_users()
 
-    # Initialize session state for page if not already done
     if "page" not in st.session_state:
-        st.session_state.page = "welcome"  # Start on the welcome page
-
-    # Check which page to display
-    if st.session_state.page == "assessment":
-        display_assessment()
-    elif st.session_state.page == "welcome":
+        st.session_state.page = "welcome"
+    if st.session_state.page == "welcome":
         welcome_page()
     elif st.session_state.page == "login":
         login_page(users)
+    # Add more page conditions as needed
 
 if __name__ == "__main__":
     main()
