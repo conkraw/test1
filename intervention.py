d0186740f2d28e53d9e15c32e156aab15e9502dd
conkraw <conkraw@gmail.com>
2024-09-28 20:06:02 -0400

    Update intervention.py

--- intervention.py
+++ intervention.py
@@ -4,16 +4,10 @@ def upload_intervention():
     st.title("Intervention Description Entry")
     interventions = st.text_area("Interventions Description", height=200)
 
-    if st.button("Upload Intervention"):
+    if st.button("Submit Intervention"):
         if interventions:
-            # Store intervention data in session state
-            st.session_state.intervention_entry = {
-                'interventions': interventions,
-                'unique_code': st.session_state.unique_code,
-                'assessment_data': st.session_state.assessment_data,
-                'diagnoses': st.session_state.diagnoses
-            }
-            st.success("Your interventions have been accepted and are under review.")
+            st.success("Your intervention has been recorded. Moving to the virtual patient...")
+            st.session_state.page = "virtual_patient"  # Navigate to virtual patient page
         else:
-            st.error("Please enter a description of the interventions.")
+            st.error("Please enter a description of the intervention.")
 
