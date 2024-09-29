41cc57c9544986c8e99a6dd399710f9c233d624b
conkraw <conkraw@gmail.com>
2024-09-28 21:00:27 -0400

    Update file_operations.py

--- utils/file_operations.py
+++ utils/file_operations.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import streamlit as st
 
+@st.cache_data
 def load_users():
+    """Load user data from CSV."""
     return pd.read_csv('users.csv')
 
 def read_text_file(txt_file_path):
+    """Read text from a TXT file."""
     try:
         with open(txt_file_path, 'r') as file:
             return file.read()
@@ -16,14 +19,15 @@ def read_text_file(txt_file_path):
         return ""
 
 def load_vital_signs(vital_signs_file):
+    """Load vital signs from a TXT file."""
     vital_signs = {}
     try:
         with open(vital_signs_file, 'r') as file:
             for line in file:
                 parts = line.strip().split(',')
                 if len(parts) == 2:
-                    key = parts[0].strip()  # Get type (e.g., heart_rate)
-                    value = parts[1].strip()  # Get vital sign description
+                    key = parts[0].strip()
+                    value = parts[1].strip()
                     vital_signs[key] = value
     except FileNotFoundError:
         st.error(f"File not found: {vital_signs_file}. Please check the file path.")
@@ -33,6 +37,7 @@ def load_vital_signs(vital_signs_file):
     return vital_signs
 
 def read_diagnoses_from_file():
+    """Read diagnoses from a TXT file."""
     try:
         with open('dx_list.txt', 'r') as file:
             diagnoses = [line.strip() for line in file.readlines() if line.strip()]
