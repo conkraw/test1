fd0f3a3356e093588d74e1edb7a5cac40cb03758
conkraw <conkraw@gmail.com>
2024-09-28 21:24:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 # app.py
 
 import streamlit as st
-from utils.file_operations import load_users
 from utils.welcome import welcome_page
 from utils.login import login_page
 from utils.intake_form import display_intake_form
 from utils.diagnoses import display_diagnoses
 
-st.set_page_config(layout="wide")
-
+# Main function to control the app flow
 def main():
-    # Load user data
-    users = load_users()
-
-    # Initialize session state for page if not already done
     if "page" not in st.session_state:
-        st.session_state.page = "welcome"  # Start on the welcome page
+        st.session_state.page = "welcome"  # Default page
 
-    # Check which page to display
-    if st.session_state.page == "assessment":
-        display_intake_form()
-    elif st.session_state.page == "welcome":
+    if st.session_state.page == "welcome":
         welcome_page()
     elif st.session_state.page == "login":
+        users = load_users()  # Ensure you have this function
         login_page(users)
+    elif st.session_state.page == "intake_form":
+        display_intake_form()
     elif st.session_state.page == "diagnoses":
         display_diagnoses()
 
