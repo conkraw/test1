f0d51d641fd403a2d9a9533495b479fdeef790ac
conkraw <conkraw@gmail.com>
2024-09-29 20:49:29 -0400

    Update simple_success.py

--- utils/simple_success.py
+++ utils/simple_success.py
@@ -8,4 +8,4 @@ def display_simple_success():
     if st.button("Proceed to Next Page"):
         print("Proceed button clicked.")  # Debugging statement
         st.session_state.page = "next_page"  # Change this to the actual next page you want to navigate to
-        st.experimental_rerun()  # Rerun the app to reflect the page change
+        st.rerun()  # Rerun the app to reflect the page change
