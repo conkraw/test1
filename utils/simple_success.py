7270060550da5f7334157f619655918b9e50698c
conkraw <conkraw@gmail.com>
2024-09-29 20:49:44 -0400

    Update simple_success.py

--- utils/simple_success.py
+++ utils/simple_success.py
@@ -3,7 +3,7 @@ import streamlit as st
 def display_simple_success():
     print("Displaying Simple Success page.")  # Debugging statement
     st.title("Success!")
-    st.write("Your operation was successful.")
+    st.write("Your operation was successful1111.")
     
     if st.button("Proceed to Next Page"):
         print("Proceed button clicked.")  # Debugging statement
