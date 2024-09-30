5d9d7132175a8178a78e74c47d6c0b78827d75a5
conkraw <conkraw@gmail.com>
2024-09-29 20:07:53 -0400

    Update simple_success.py

--- utils/simple_success.py
+++ utils/simple_success.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
 def display_simple_success():
+    print("Displaying Simple Success page.")  # Debugging statement
     st.title("Success!")
     st.write("Your operation was successful.")
     
     if st.button("Proceed to Next Page"):
+        print("Proceed button clicked.")  # Debugging statement
         st.session_state.page = "next_page"  # Change this to the actual next page you want to navigate to
-        st.rerun()  # Rerun the app to reflect the page change
+        st.experimental_rerun()  # Rerun the app to reflect the page change
