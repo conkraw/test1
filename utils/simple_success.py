49987ac0d4d4cdd7da9d0a5a4654d530aab4be80
conkraw <conkraw@gmail.com>
2024-09-29 20:01:56 -0400

    Update simple_success.py

--- utils/simple_success.py
+++ utils/simple_success.py
@@ -10,7 +10,7 @@ def display_simple_success():
         # Button to go to the next page
         if st.button("Go to Next Page"):
             st.session_state.current_page = "next_page"  # Change this to your actual next page
-            st.experimental_rerun()  # Rerun the app to reflect the change
+            st.rerun()  # Rerun the app to reflect the change
 
 if __name__ == "__main__":
     display_simple_success()
