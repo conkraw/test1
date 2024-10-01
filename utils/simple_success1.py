f099d17916087dd01ce4c9329b6775ea4df443e8
conkraw <conkraw@gmail.com>
2024-09-30 21:10:24 -0400

    Update simple_success1.py

--- utils/simple_success1.py
+++ utils/simple_success1.py
@@ -5,7 +5,7 @@ def display_simple_success1():
     st.title("Success!")
     st.write("Your operation was successful1111.")
     
-    if st.button("Proceed to Next Page"):
-        print("Proceed button clicked.")  # Debugging statement
-        st.session_state.page = "NEXT PAGE"  # Change this to the actual next page you want to navigate to
-        st.rerun()  # Rerun the app to reflect the page change
+    if st.button("Final Submit"):
+    session_data = collect_session_data()
+    upload_message = upload_to_firebase(db, session_data)
+    st.success(upload_message) 
