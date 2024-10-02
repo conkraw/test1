3bc9eb5b65abae9fe65a1401b007d1f28e78d833
conkraw <conkraw@gmail.com>
2024-10-02 17:35:12 -0400

    Update session_management.py

--- utils/session_management.py
+++ utils/session_management.py
@@ -6,11 +6,11 @@ def collect_session_data():
     session_data = {
         "unique_code": st.session_state.get("unique_code", ""),
         "vs_data": st.session_state.get("vs_data", {}),
-        "diagnoses_s1": st.session_state.get("diagnoses_s1", []),
+        #"diagnoses_s1": st.session_state.get("diagnoses_s1", []),
         "interventions": st.session_state.get("interventions", []),
         "questions_asked": st.session_state.get("questions_asked", []),
         "responses": st.session_state.get("responses", []),
-        "examinations": st.session_state.get("examinations", []),
+        #"examinations": st.session_state.get("examinations", []),
         # Add other session state variables as needed
     }
     return session_data
