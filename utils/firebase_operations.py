f2a7c42b311077e5f2e86792a556e339ab4c8677
conkraw <conkraw@gmail.com>
2024-10-01 21:40:09 -0400

    Update firebase_operations.py

--- utils/firebase_operations.py
+++ utils/firebase_operations.py
@@ -1,33 +1,27 @@
-import pandas as pd
+# firebase_operations.py
 import firebase_admin
 from firebase_admin import credentials, firestore
-import streamlit as st
 import os
 import json
 
 # Initialize Firebase
 def initialize_firebase():
-    # Load Firebase credentials from environment variable
     FIREBASE_KEY_JSON = os.getenv('FIREBASE_KEY')
 
     if FIREBASE_KEY_JSON is None:
-        st.error("FIREBASE_KEY environment variable not set.")
-        return None
+        raise ValueError("FIREBASE_KEY environment variable not set.")
 
     try:
-        # Parse the JSON string into a dictionary
         firebase_credentials = json.loads(FIREBASE_KEY_JSON)
 
-        # Initialize Firebase only if not already initialized
         if not firebase_admin._apps:
             cred = credentials.Certificate(firebase_credentials)
             firebase_admin.initialize_app(cred)
 
-        # Get Firestore client
         return firestore.client()
     except Exception as e:
-        st.error(f"Error initializing Firebase: {e}")
-        return None
+        raise Exception(f"Error initializing Firebase: {e}")
+
 
 #def upload_to_firebase(db, entry):
 #    db.collection('your_collection_name').add(entry)  # Change 'your_collection_name' to your collection name
