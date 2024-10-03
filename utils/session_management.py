fe8765042788ad924a978178878b5af52e1f1c52
conkraw <conkraw@gmail.com>
2024-10-03 11:09:35 -0400

    Update session_management.py

--- utils/session_management.py
+++ utils/session_management.py
@@ -1,32 +1,22 @@
-# utils/session_management.py
-
 import streamlit as st
 
-#def collect_session_data():
-    #session_data = {
-        #"unique_code": st.session_state.get("unique_code", ""),
-        #"vs_data": st.session_state.get("vs_data", {}),
-        #"diagnoses_s1": st.session_state.get("diagnoses_s1", []),
-        #"interventions": st.session_state.get("interventions", []),
-        #"questions_asked": st.session_state.get("questions_asked", []),
-        #"responses": st.session_state.get("responses", []),
-        #"examinations": st.session_state.get("examinations", []),
-        # Add other session state variables as needed
-    #}
-    #return session_data
-
-import streamlit as st
+def initialize_session():
+    if 'page' not in st.session_state:
+        st.session_state.page = "welcome"
+    if 'unique_code' not in st.session_state:
+        st.session_state.unique_code = None
+    if 'user_name' not in st.session_state:
+        st.session_state.user_name = None
+    if 'vs_data' not in st.session_state:
+        st.session_state.vs_data = None
 
 def collect_session_data():
     return {
-        'page': st.session_state.page,
-        'diagnoses': st.session_state.get('diagnoses', []),
-        'treatments': st.session_state.get('treatments', []),
-        # Add other fields as necessary
+        'unique_code': st.session_state.get('unique_code'),
+        'user_name': st.session_state.get('user_name'),
+        'vs_data': st.session_state.get('vs_data'),
+        'page': st.session_state.get('page'),
     }
-
-
-
 
 
 
