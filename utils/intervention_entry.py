c7ff0d4a59617736ca7c620068a56b177c49143f
conkraw <conkraw@gmail.com>
2024-10-03 14:33:07 -0400

    Update intervention_entry.py

--- utils/intervention_entry.py
+++ utils/intervention_entry.py
@@ -27,7 +27,6 @@ def main(db,document_id):
                 "interventions": interventions}
             
             # Upload the session data to Firebase
-            #upload_message = upload_to_firebase(db, 'your_collection_name', document_id, entry)  # Upload to Firebase
             upload_message = upload_to_firebase(db, document_id, entry)
             
             st.success("Your interventions have been saved successfully.")
