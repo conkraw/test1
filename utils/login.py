4d7aef5c0ae2a0b4318917e9cb1da7839e4b86bd
conkraw <conkraw@gmail.com>
2024-10-03 22:08:26 -0400

    Update login.py

--- utils/login.py
+++ utils/login.py
@@ -23,7 +23,7 @@ def login_page(users, db, document_id):  # Accept document_id as a parameter
                 #last_page = load_last_page(db, st.session_state.document_id)
                 
                 # Set the last_page in session state and navigate the user to that page
-                st.session_state.page = last_page
+                #st.session_state.page = last_page
 
                 st.success(f"Welcome back! Redirecting to {last_page}...")
                 st.rerun()  # Rerun to refresh and redirect to the correct page
