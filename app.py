809f8f741ebb63d244481354a496101a9fa8bb70
conkraw <conkraw@gmail.com>
2024-10-03 20:35:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,47 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+from utils.file_operations import load_users
+from utils.welcome import welcome_page
+from utils.login import login_page
+from utils.intake_form import display_intake_form
+from utils.diagnoses import display_diagnoses
+from utils.intervention_entry import main as intervention_entry_main
+from utils.history_with_ai import run_virtual_patient
+from utils.focused_physical_examination import display_focused_physical_examination
+from utils.physical_examination import main as display_physical_examination
+from utils.history_illness_script import main as history_illness_script
+from utils.simple_success import display_simple_success
+from utils.simple_success1 import display_simple_success1
+from utils.physical_examination_features import display_physical_examination_features
+from utils.lab_tests import display_laboratory_tests
+from utils.radtests import display_radiological_tests
+from utils.othertests import display_other_tests
+from utils.results import display_results_image
+from utils.laboratory_features import display_laboratory_features
+from utils.treatments import display_treatments
+from utils.firebase_operations import initialize_firebase, upload_to_firebase
+from utils.session_management import collect_session_data
+import uuid  # To generate unique document IDs
+
+def save_user_state(db):
+    if st.session_state.user_code:
+        entry = {
+            "last_page": st.session_state.page,
+            # Add other session data if needed
+        }
+        upload_to_firebase(db, st.session_state.user_code, entry)
+
+def load_last_page(db):
+    collection_name = st.secrets["FIREBASE_COLLECTION_NAME"]  # Get collection name from secrets
+    if st.session_state.user_code:
+        user_data = db.collection(collection_name).document(st.session_state.user_code).get()
+        if user_data.exists:
+            return user_data.to_dict().get("last_page")
+    return "welcome"
+
+
 def main():
     # Initialize Firebase
     db = initialize_firebase()
